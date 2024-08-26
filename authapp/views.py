097d6ca767e475eedbdf615ffa5216681d6356a4
OMPRAKASH MANIVANNAN <ommanivannan@aol.com>
2024-08-26 11:35:18 +0530

    V1.0 - works fine @ Aug 26 11:35 AM

---
+++ authapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or another URL
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to the login page or another URL
+
+
+def home_view(request):
+    return HttpResponse("Welcome to the home page!")
+
