c1310157ccf7c972b517d3cf22a38b6b3bbca5e4
OMPRAKASH MANIVANNAN <ommanivannan@aol.com>
2024-08-26 23:37:24 +0530

    V1.0.3 - Login, Success and error html with aesthetic looks added

--- authapp/views.py
+++ authapp/views.py
@@ -2,20 +2,35 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
+from django.contrib import auth, messages
 
 
 def login_view(request):
+    uname = None
+    upass = None
+
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
+        uname = request.POST.get('username')
+        upass = request.POST.get('password')
+        
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('home')  # Redirect to a home page or another URL
-        else :
-                return render(request, 'error.html', {'form': form})
+        else:
+            print(f" OM:::>>> user password = {uname}, password = {upass}")
+            return render(request, 'error.html', {
+                'user': uname,
+                'password': upass,
+                'form': form
+            })
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
+
+
 
 def logout_view(request):
     logout(request)
@@ -23,5 +38,8 @@ def logout_view(request):
 
 
 def home_view(request):
-    return HttpResponse("Welcome to the home page!")
+    return render(request, 'success.html')
+
+        # return HttpResponse("Welcome to the home page!")
+
 
