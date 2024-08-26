ffd4010daf47b969ba974b857a7fea201ea02856
OMPRAKASH MANIVANNAN <ommanivannan@aol.com>
2024-08-26 11:41:53 +0530

    V1.0.1 : Direct to Home page

--- droid/urls.py
+++ droid/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('', include('authapp.urls')),  # Include your app's URLs
+    path('', include('authapp.urls')),  # Include your app's URLs
 ]
